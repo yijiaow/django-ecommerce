4dd397381954a5684063ac4b7c9250c1c000f277
Yijiao Wang <ttwyj250@gmail.com>
2020-12-01 23:20:39 -0800

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY_CHOICES = ()
+
+LABEL_CHOICES = ()
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+    def get_total(self):
+        return self.item.price * self.quantity
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def shipping(self):
+        pass
+
+    @property
+    def cart_total(self):
+        total = sum([item.get_total() for item in self.items])
+        return total
--- core/views.py
+++ core/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView
+from django.http import HttpResponse, JsonResponse
+
+from .models import Product, OrderItem, Order
+
+
+class StoreView(ListView):
+    model = Product
+    template_name = 'store.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product.html'
+
+
+class CartView(DetailView):
+    model = OrderItem
+    template_name = 'cart.html'
+
+
+class CheckoutView(View):
+    pass
 
 
 def store(request):
