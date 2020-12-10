7cbf52559af0d1610ce604ed6eff2b7ce00430d5
Yijiao Wang <ttwyj250@gmail.com>
2020-12-10 11:28:00 -0800

    Build custom number stepper form widget (resolve #1), refactor class based views (resolve #3)

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from .widgets import NumberStepperWidget
+
 
 class ProductQuantityForm(forms.Form):
-    quantity = forms.IntegerField(required=True)
+    quantity = forms.IntegerField(
+        required=False, initial=1, label='', widget=NumberStepperWidget(attrs={'min': 1, 'max': 10}))
