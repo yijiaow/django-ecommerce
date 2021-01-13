0314eafd79e5b2f4a6a4f010b825b4484c23ac06
Yijiao Wang <ttwyj250@gmail.com>
2021-01-12 22:46:28 -0800

    Send billing form to backend once Stripe payment succeeds

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,12 @@ from .widgets import NumberStepperWidget
 class ProductQuantityForm(forms.Form):
     quantity = forms.IntegerField(
         required=False, initial=1, label='', widget=NumberStepperWidget(attrs={'min': 1, 'max': 10}))
+
+
+class BillingForm(AddressForm):
+    same_as_shipping = forms.BooleanField(
+        required=False, initial=True, label='Billing address is the same as shipping address')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['same_as_shipping'].widget.attrs['class'] = 'form-control-input'
